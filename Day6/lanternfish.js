dc1ce26527ad187536ba681e7dadeb2b0ef4d3aa
Jeremy Hahn <jeremydhahn@gmail.com>
2021-12-09 18:03:41 -0500

    Day 6 complete. Tried using an exponential growth equation but that was taking way too long

--- Day6/lanternfish.js
+++ Day6/lanternfish.js
@@ -1,4 +1,4 @@
-export const lanternfish = (input) => {
+export const lanternfishPart1 = (input, duration) => {
   let day = 0;
   let lanternfishList = input.split(',').map((fish) => parseInt(fish));
 
@@ -18,10 +18,82 @@ export const lanternfish = (input) => {
     }
   };
 
-  while (day < 80) {
+  while (day < duration) {
+    console.log('day: ', day);
     calculateLanternfish();
     day++;
   }
 
   return lanternfishList.length;
+};
+
+// export const lanternfishPart2 = (
+//   originalInput,
+//   endingFish,
+//   originalDuration,
+//   testDuration
+// ) => {
+//   let origInput = originalInput.split(',').map((fish) => parseInt(fish));
+//   const sumOriginalInput = origInput.reduce((acc, d) => {
+//     acc += d;
+//     return acc;
+//   }, 0);
+//   const avgOriginalInput = sumOriginalInput / origInput.length;
+//   let originalDurationAdjusted = originalDuration - avgOriginalInput;
+//   const partialFishAtBeginning = sumOriginalInput / 7;
+//   const startingFish = origInput.length;
+//   let simplify = endingFish / startingFish;
+//   let naturalLog = Math.log(simplify);
+//   let expGrowthRate = naturalLog / originalDuration;
+//   const totalFishCount =
+//     startingFish * Math.pow(1 + expGrowthRate, testDuration);
+//   const totaltotal = Math.floor(totalFishCount + partialFishAtBeginning);
+
+//   return totaltotal;
+// };
+
+export const lanternfishPart2 = (input, duration) => {
+  let day = 0;
+  const lanternfishList = input.split(',').map((fish) => parseInt(fish));
+  let fishCount = lanternfishList.length;
+  let fishList = [
+    { count: 0, new: false },
+    { count: 0, new: false },
+    { count: 0, new: false },
+    { count: 0, new: false },
+    { count: 0, new: false },
+    { count: 0, new: false },
+    { count: 0, new: false },
+    { count: 0, new: false },
+    { count: 0, new: false },
+    { count: 0, new: false },
+  ];
+  lanternfishList.forEach((fish) => {
+    fishList[fish].count++;
+  });
+
+  const calculateLanternfish = () => {
+    fishList.forEach((fish, index) => {
+      if (index === 0 && fish.count > 0) {
+        fishList[9].count += fish.count;
+        fishList[9].new = true;
+        fishCount += fish.count;
+        fishList[7].count += fish.count;
+        fishList[7].new = false;
+        fishList[0].count = 0;
+      } else if (index !== 0 && fish.count > 0) {
+        fishList[index - 1].count += fish.count;
+        fishList[index - 1].new = fish.new;
+        fishList[index].count = 0;
+        fishList[index].new = false;
+      }
+    });
+  };
+
+  while (day < duration) {
+    calculateLanternfish();
+    day++;
+  }
+
+  return fishCount;
 };
--- Day6/lanternfish.test.js
+++ Day6/lanternfish.test.js
@@ -1,16 +1,41 @@
 import assert from 'assert';
-import { lanternfish } from './lanternfish.js';
-import { input } from './input.js';
+import { lanternfishPart1, lanternfishPart2 } from './lanternfish.js';
+import { testInput } from './input.js';
 
 describe('Day 6: Lanternfish', function () {
   describe('Part 1', function () {
     it('should return the number lanternfish after 80 days', async function () {
       const input = '3,4,3,1,2';
-      assert.equal(lanternfish(input), 5934);
+      assert.equal(lanternfishPart2(input, 80), 5934);
     });
 
     it('should return the number lanternfish after 80 days with real input', async function () {
-      console.log('Answer: ', lanternfish(input));
+      //Answer:  349549
+      console.log('Answer: ', lanternfishPart2(testInput, 80));
+    });
+
+    it('should return the number lanternfish after 80 days with real input', async function () {
+      //Answer:  10396
+      console.log('Answer: ', lanternfishPart1(testInput, 40));
+    });
+
+    it('should return the number lanternfish after 80 days with real input', async function () {
+      //Answer:  1994508
+      console.log('Answer: ', lanternfishPart2(testInput, 100));
+    });
+  });
+
+  describe('Part 2', function () {
+    it.skip('should return the number lanternfish after 18 days with exponential growth rate calculation', async function () {
+      assert.equal(lanternfishPart2('3,4,3,1,2', 26984457539, 256, 18), 26);
+    });
+
+    it.skip('should return the number lanternfish after 80 days with exponential growth rate calculation', async function () {
+      assert.equal(lanternfishPart2(testInput, 1994508, 100, 80), 349549);
+    });
+
+    it.only('should return the number lanternfish after 256 days with real input', async function () {
+      console.log('Answer: ', lanternfishPart2(testInput, 256));
     });
   });
 });
