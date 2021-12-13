08077dfee220785e5983854fd6386db68bfdd24c
Jeremy Hahn <jeremydhahn@gmail.com>
2021-12-13 11:31:20 -0500

    Day 10 complete

--- Day10/syntaxScoring.js
+++ Day10/syntaxScoring.js
@@ -1,8 +1,7 @@
 const reverseMatch = { ')': '(', ']': '[', '}': '{', '>': '<' };
 
-export const syntaxScoring = (input) => {
+export const syntaxScoringPart1 = (input) => {
   const inputParsed = input.split('\n').map((i) => i.trim().split(''));
-  const matchingCharacters = { '(': ')', '[': ']', '{': '}', '<': '>' };
   let syntaxErrors = [];
   const scoring = { ')': 3, ']': 57, '}': 1197, '>': 25137 };
 
@@ -19,10 +18,33 @@ export const syntaxScoring = (input) => {
   return score;
 };
 
+export const syntaxScoringPart2 = (input) => {
+  const inputParsed = input.split('\n').map((i) => i.trim().split(''));
+  const matchingCharacters = { '(': ')', '[': ']', '{': '}', '<': '>' };
+  const scoring = { ')': 1, ']': 2, '}': 3, '>': 4 };
+  let syntaxIncompleteScores = [];
+
+  inputParsed.forEach((input) => {
+    const cleaned = removeEmptyTags(input);
+    const hasIncompleteSyntax = cleaned.every((c) => matchingCharacters[c]);
+    if (hasIncompleteSyntax) {
+      const score = cleaned.reverse().reduce((totalScore, err) => {
+        totalScore = totalScore * 5 + scoring[matchingCharacters[err]];
+        return totalScore;
+      }, 0);
+      syntaxIncompleteScores.push(score);
+    }
+  });
+  const middleScore = syntaxIncompleteScores.sort((a, b) => a - b)[
+    Math.floor(syntaxIncompleteScores.length / 2)
+  ];
+  return middleScore;
+};
+
 const removeEmptyTags = (dirty) => {
   const dirtyLength = dirty.length - 1;
   const emptyTagsRemoved = dirty.reduceRight((acc, curr, index) => {
-    if (reverseMatch[curr] === dirty[index - 1]) {
+    if (index > 0 && reverseMatch[curr] === dirty[index - 1]) {
       acc.splice(index - 1, 2);
     }
     return acc;
--- Day10/syntaxScoring.test.js
+++ Day10/syntaxScoring.test.js
@@ -1,14 +1,23 @@
 import assert from 'assert';
-import { syntaxScoring } from './syntaxScoring.js';
+import { syntaxScoringPart1, syntaxScoringPart2 } from './syntaxScoring.js';
 import { testInput, realInput } from './input.js';
 
 describe('Day 10: Syntax Scoring', function () {
   describe('Part 1', function () {
     it('should return the total syntax error score', async function () {
-      assert.equal(syntaxScoring(testInput), 26397);
+      assert.equal(syntaxScoringPart1(testInput), 26397);
     });
     it('should return the total syntax error score with real input', async function () {
-      console.log('Answer: ', syntaxScoring(realInput));
+      console.log('Answer: ', syntaxScoringPart1(realInput));
+    });
+  });
+
+  describe('Part 2', function () {
+    it('should return the middle syntax error score', async function () {
+      assert.equal(syntaxScoringPart2(testInput), 288957);
+    });
+    it('should return the middle syntax error score with real input', async function () {
+      console.log('Answer: ', syntaxScoringPart2(realInput));
     });
   });
 });
